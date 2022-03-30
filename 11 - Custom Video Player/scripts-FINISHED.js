333b90c2c5f28ac921c93573b7bda919178ac692
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-03-30 13:39:52 -0400

    add handleProgress() function

--- 11 - Custom Video Player/scripts-FINISHED.js
+++ 11 - Custom Video Player/scripts-FINISHED.js
@@ -30,3 +30,9 @@ function skip() {
 function handleRangeUpdate() {
   video[this.name] = this.value;
 }
+
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
