005923020efbce0cdedecdd468be1a3718266a42
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-03-30 13:36:41 -0400

    add updateButton() function

--- 11 - Custom Video Player/scripts-FINISHED.js
+++ 11 - Custom Video Player/scripts-FINISHED.js
@@ -13,3 +13,10 @@ function togglePlay() {
   const method = video.paused ? 'play' : 'pause';
   video[method]();
 }
+
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon);
+  toggle.textContent = icon;
+}
