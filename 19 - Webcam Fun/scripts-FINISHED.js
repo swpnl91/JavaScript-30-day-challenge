84f5f725122f1bfdeaeb84ecf73ed96650052979
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-04-08 20:16:31 -0400

    create function takePhoto

--- 19 - Webcam Fun/scripts-FINISHED.js
+++ 19 - Webcam Fun/scripts-FINISHED.js
@@ -38,3 +38,17 @@ function paintToCanvas() {
     ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
+
+function takePhoto() {
+  // played the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // take the data out of the canvas
+  const data = canvas.toDataURL('image/jpeg');
+  const link = document.createElement('a');
+  link.href = data;
+  link.setAttribute('download', 'handsome');
+  link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
+  strip.insertBefore(link, strip.firstChild);
+}
