4c5bf6d985a93019982c250bf1c5b8cf92124777
Swapnil (Neil) Parab <swapnil.parab2013@gmail.com>
2022-04-21 08:39:48 -0400

    create function displayEndTime

--- 29 - Countdown Timer/scripts-FINISHED.js
+++ 29 - Countdown Timer/scripts-FINISHED.js
@@ -31,3 +31,11 @@ function displayTimeLeft(seconds) {
   document.title = display;
   timerDisplay.textContent = display;
 }
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
